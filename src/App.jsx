b0b92bc4432f4a2ac5b47ed960cc4cf43d03e8e1
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2025-05-23 14:51:32 +0530

    added push filter

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Github Actions</h1>
+    <> 
+      <h1> Learn Github Actions</h1>
       <h2>Complete course with 2 projects</h2>
     </>
   )
